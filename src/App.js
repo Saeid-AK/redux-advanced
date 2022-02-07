986063c338499c303971469eb0c401b6e247662a
Saeid-AK <saeidak384@hotmail.com>
2022-02-07 15:40:09 +0330

    added async action inside the component using useEffect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,73 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { uiActions } from './redux/reducers/ui-slice';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
 
 function App() {
+  const dispatch = useDispatch();
   const toggleCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+  const notification = useSelector(state => state.ui.notification);
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'pending',
+          title: 'Sendig...',
+          message: 'Sending cart data!',
+        })
+      );
+      const response = await fetch(
+        `https://productsdb-7026a-default-rtdb.firebaseio.com/cart.json`,
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed');
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Sending cart data successfully!',
+        })
+      );
+    };
+    sendCartData().catch(error => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending cart data failed!',
+        })
+      );
+    });
+  }, [cart]);
 
   return (
-    <Layout>
-      {toggleCart && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {toggleCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
