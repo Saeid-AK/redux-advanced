8e859e03699329667d47144cfcb69a81723b0d0c
Saeid-AK <saeidak384@hotmail.com>
2022-02-10 23:27:13 +0330

    created seperate file for actions and added fetch cart data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { uiActions } from './redux/reducers/ui-slice';
+import { sendCartData, fetchCartData } from './redux/reducers/cart-actions';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
+
+let isInitial = true;
 
 function App() {
   const dispatch = useDispatch();
@@ -15,44 +17,19 @@ function App() {
   const notification = useSelector(state => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'pending',
-          title: 'Sendig...',
-          message: 'Sending cart data!',
-        })
-      );
-      const response = await fetch(
-        `https://productsdb-7026a-default-rtdb.firebaseio.com/cart.json`,
-        {
-          method: 'PUT',
-          body: JSON.stringify(cart),
-        }
-      );
+    dispatch(fetchCartData());
+  }, [dispatch]);
 
-      if (!response.ok) {
-        throw new Error('Sending cart data failed');
-      }
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
 
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'Success!',
-          message: 'Sending cart data successfully!',
-        })
-      );
-    };
-    sendCartData().catch(error => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Sending cart data failed!',
-        })
-      );
-    });
-  }, [cart]);
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
+  }, [cart, dispatch]);
 
   return (
     <>
